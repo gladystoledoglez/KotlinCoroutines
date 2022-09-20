b584d3a2e79a0ecfa608f6276b6313de714ef998
Gladys María Toledo González <gladysmtg1999@gmail.com>
2022-09-20 13:02:08 -0400

    Using async and await

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,24 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            Log.d(TAG, "Starting long running calculation...")
-            withTimeout(3000L) {
-                for (i in 30..40) {
-                    if (isActive) {
-                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
-                    }
-                }
+        GlobalScope.launch(Dispatchers.Default) {
+            val time = measureTimeMillis {
+                val answer1 = async { networkCall1() }
+                val answer2 = async { networkCall2() }
+                Log.d(TAG, "Answer1 is ${answer1.await()}")
+                Log.d(TAG, "Answer2 is ${answer2.await()}")
             }
-            Log.d(TAG, "Ending long running calculation...")
+            Log.d(TAG, "Request took $time ms.")
         }
     }
 
-    fun fib(n: Int): Long {
-        return when (n) {
-            0 -> 0
-            1 -> 1
-            else -> fib(n - 1) + fib(n - 2)
-        }
+    suspend fun networkCall1(): String {
+        delay(3000L)
+        return "Answer 1"
+    }
+
+    suspend fun networkCall2(): String {
+        delay(3000L)
+        return "Answer 2"
     }
 }
